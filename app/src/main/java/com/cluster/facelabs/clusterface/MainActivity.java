d0e3d614bd828b33896e8e12a2253368ba6f8f3a
Shankar <shankaragrawal012@gmail.com>
2019-03-10 18:02:07 +0530

    C4 : Storage permission requests

--- app/src/main/java/com/cluster/facelabs/clusterface/MainActivity.java
+++ app/src/main/java/com/cluster/facelabs/clusterface/MainActivity.java
@@ -1,11 +1,19 @@
 package com.cluster.facelabs.clusterface;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     TfliteHandler tfliteHandler;
+
+    private static final int MY_PERMISSIONS_REQUEST_STORAGE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +21,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tfliteHandler = new TfliteHandler(this, MainActivity.this);
+
+        requestPermissions();
+    }
+
+    private void requestPermissions(){
+        boolean readPermissionGranted = ContextCompat.checkSelfPermission(
+                this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED;
+        boolean writePermissionGranted = ContextCompat.checkSelfPermission(
+                this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED;
+        ArrayList<String> permissionsToRequest = new ArrayList<>();
+        if(!readPermissionGranted)
+            permissionsToRequest.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        if(!writePermissionGranted)
+            permissionsToRequest.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if(!permissionsToRequest.isEmpty()){
+            ActivityCompat.requestPermissions(this, permissionsToRequest.toArray(new String[0]),
+                    MY_PERMISSIONS_REQUEST_STORAGE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Utils.showToast(this ,"Storage permission granted!");
+                } else {
+                    Utils.showToast(this ,"Storage permission denied!");
+                }
+                return;
+            }
+        }
     }
 }
